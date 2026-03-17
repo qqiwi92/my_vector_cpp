9393cb5b5c016c9fbf65f2f6a84055fc52bc7721
Dmitry Levkin <mit.levkin@vk.com>
2026-03-17 11:12:24 +0300

    add color printing unpassed tests

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include "vector.hpp"
 #include <iostream>
+
+#define ANSI_RED "\033[31m"
+#define ANSI_GREEN "\033[32m"
+#define ANSI_RESET "\033[0m"
+
 bool testEmptyVector() { return false; }
 
 int main()
@@ -7,10 +12,22 @@ int main()
   using test_t = std::pair< const char*, bool (*)() >;
   test_t tests[] = {{"Empty vector", testEmptyVector}};
   const size_t count = sizeof(tests) / sizeof(test_t);
+
+  std::cout << std::boolalpha;
+
+  bool all_pass = true;
   for (size_t i = 0; i < count; ++i) {
-    std::cout << tests[i].first << ": ";
-    std::cout << tests[i].second() << ": ";
+    bool res = tests[i].second();
+
+    if (!res) {
+      std::cout << ANSI_RED << tests[i].first << ": " << res << ANSI_RESET;
+      all_pass = false;
+    } else {
+      std::cout << ANSI_GREEN << tests[i].first << ": " << res << ANSI_RESET;
+    }
+    std::cout << "\n";
   }
 
   std::cout << "\n";
+  return all_pass ? 0 : 1;
 }
