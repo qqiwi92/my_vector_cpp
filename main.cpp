fb1547b2d01aa061024a191743629626ec7fabb8
Dmitry Levkin <dmtrlevkn@gmail.com>
2026-03-17 11:08:52 +0300

    made a testing framework

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include "vector.hpp"
+#include <iostream>
+bool testEmptyVector() { return false; }
 
 int main()
 {
+  using test_t = std::pair< const char*, bool (*)() >;
+  test_t tests[] = {{"Empty vector", testEmptyVector}};
+  const size_t count = sizeof(tests) / sizeof(test_t);
+  for (size_t i = 0; i < count; ++i) {
+    std::cout << tests[i].first << ": ";
+    std::cout << tests[i].second() << ": ";
+  }
+
+  std::cout << "\n";
 }
