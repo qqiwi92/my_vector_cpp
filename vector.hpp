a837507ae50b9767b693c758a629279ee41fd27c
Dmitry Levkin <mit.levkin@vk.com>
2026-03-17 11:08:52 +0300

    made a testing framework

--- vector.hpp
+++ vector.hpp
@@ -3,10 +3,30 @@
 
 #include <cstddef>
 
+namespace stuff {
+
+  template < class T > struct Vector {
+    ~Vector();
+    Vector();
+    
+  public:
+    T* data_;
+    size_t size_, capacity_;
+  };
+
+}
+
+
+using namespace stuff;
+
 template <class T>
-struct Vector {
-    T* data;
-    size_t size, capacity;
+stuff::Vector<T>::~Vector() {
+    
+}
+
+template <class T>
+stuff::Vector<T>::Vector(): data_(), capacity_(), size_() {
+    
 }
 
 #endif
