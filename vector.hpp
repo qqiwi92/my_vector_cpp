c0e6bad55b94d36c2f9d62575f1e4558c5aa1d13
Dmitry Levkin <mit.levkin@vk.com>
2026-03-22 16:39:58 +0300

    expand() func

--- vector.hpp
+++ vector.hpp
@@ -19,9 +19,11 @@ namespace stuff {
 
     void pushBack(const T& v);
     void popBack();
-    void insert(size_t i, const T & v);
+    void insert(size_t i, const T& v);
     void erase(size_t i);
+
   private:
+    void expand();
     T* data_;
     size_t size_, capacity_;
   };
@@ -40,6 +42,25 @@ template < class T > bool stuff::Vector< T >::isEmpty() const noexcept
   return !size_;
 }
 
-// template < class T > bool stuff::Vector< T >::isEmpty() const noexcept
+template < class T > void stuff::Vector< T >::expand()
+{
+  size_t nw_capacity = 3 / 2 * (capacity_ + 1);
+  T* nw = nullptr;
+  try {
+
+    nw = new T[nw_capacity];
+    for (size_t i = 0; i < size_; ++i) {
+      nw[i] = data_[i];
+    }
+    delete[] data_;
+    data_ = nw;
+    capacity_ = nw_capacity;
+    
+  } catch (...) {
+    delete[] nw;
+  }
+}
+
+template < class T > void stuff::Vector< T >::pushBack(const T& val) {}
 
 #endif
